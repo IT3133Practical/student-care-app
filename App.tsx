9017a999533a056ce7609b9a01ba4e6449c6fee2
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-01-05 23:34:08 +0530

    Implement navigation stack with Login and BottomTabNavigator screens

--- App.tsx
+++ App.tsx
@@ -1,22 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Footer from './src/components/footer/Footer';
+import React, { useState } from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import BottomTabNavigator from './src/components/navigator/BottomTabNavigator'
+import Login from './src/screen/login/Login'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Footer/>
-    </View>
-  );
+type RootStackParamList = {
+  Login: undefined
+  BottomTabs: { user: any }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator<RootStackParamList>()
+
+export default function App() {
+  const [user, setUser] = useState<any>(null)
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: true,
+          headerTintColor: 'white',
+          headerTitle: 'UoV Student Care',
+          headerStyle: {
+            backgroundColor: '#4b0150',
+          },
+        }}
+      >
+        <Stack.Screen name="Login">
+          {props => <Login {...props} setUser={setUser} />}
+        </Stack.Screen>
+        <Stack.Screen name="BottomTabs" component={BottomTabNavigator} initialParams={{ user }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
