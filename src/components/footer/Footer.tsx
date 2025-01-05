eb316fb71c7a47ad2547cbd712de904cb2dcdd1e
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-01-05 22:58:03 +0530

    Add Footer component with styling and copyright text

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Footer() {
+    return (
+        <View style={{ backgroundColor: '#4b0150', width: '100%', alignItems: 'center', justifyContent: 'center', margin: 20 }} >
+            <Text style={{ color: '#fff', padding: 20 }}>UoV © 2025</Text>
+        </View>
+    )
+}
